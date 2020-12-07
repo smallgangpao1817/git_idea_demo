0b86fdb6f88d324aeafefbb49f1c24b9cd692176
wugang <443662824@qq.com>
2020-12-07 15:35:22 +0800

    this is test git5

--- src/main/java/com/example/demo/domain/User.java
+++ src/main/java/com/example/demo/domain/User.java
@@ -10,5 +10,6 @@ public class User {
     public static void main(String[] args) {
         System.out.println("hahaha");
         System.out.println("jaj");
+        System.out.println("jdfjdf");
     }
 }
