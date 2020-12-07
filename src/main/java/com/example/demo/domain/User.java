cb4ffc54d595dbdc66949a45a834b1c68de22f6a
wugang <443662824@qq.com>
2020-12-07 15:07:48 +0800

    this is test git

---
+++ src/main/java/com/example/demo/domain/User.java
@@ -0,0 +1,13 @@
+package com.example.demo.domain;
+
+/**
+ * @author small
+ * @date 2020-12-07 15:05
+ * @description User
+ */
+
+public class User {
+    public static void main(String[] args) {
+        System.out.println("hahaha");
+    }
+}
